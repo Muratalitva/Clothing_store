cbc82d8427f26fc2bf1c9958e7840f1c65b8c689
Muratalieva <20adinka09@gmail.com>
2023-10-07 20:30:55 +0300

    shop

---
+++ settings/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from settings.models import Settings,About,Teach
+# Register your models here.
+admin.site.register(Settings)
+admin.site.register(About)
+admin.site.register(Teach)
